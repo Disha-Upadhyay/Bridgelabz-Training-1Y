1d3672fac44d8b65abcc0670c0345fae80e20370
Disha-Upadhyay <dishaupadhyay2007@gmail.com>
2026-01-12 22:55:48 +0530

    Demo1.java

---
+++ Demo1.java
@@ -0,0 +1,23 @@
+public class Demo1{
+static int x = 19;
+int z = 20;
+public static void m2(){
+    int y = 10;
+	System.out.println(y);
+}
+public static void m1(){
+    int y = 30;
+	System.out.println(y);
+	m2();
+}
+public static void main(String[] args){
+    int y = 40;
+	System.out.println("HelloWorld");
+	System.out.println(x);
+	System.out.println(y);
+	m1();
+	Demo1 obj = new Demo1();
+	System.out.println(obj.z);
+}
+}
+    
