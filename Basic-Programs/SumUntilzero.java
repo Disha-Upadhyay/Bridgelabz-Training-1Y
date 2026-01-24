a96687d3a9004cf4e04ca316741b16af3aa41236
Disha-Upadhyay <dishaupadhyay2007@gmail.com>
2026-01-24 21:30:02 +0530

    commit

---
+++ Basic-Programs/SumUntilzero.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SumUntilzero{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+double total = 0.0;
+System.out.println("Enter a number:");
+double number = sc.nextDouble();
+while(number!=0){
+total = total + number;
+System.out.println("Enter a number:");
+number = sc.nextDouble();
+}
+System.out.println("Sum of numbers: "+total);
+}
+}
+
