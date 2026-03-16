56c2921e74a3d412f67de3ff635a67e3514b6d4b
Disha-Upadhyay <dishaupadhyay2007@gmail.com>
2026-03-16 16:06:05 +0530

    first commit

---
+++ Assignment/Smart Library System/src/com/gla/TextBook.java
@@ -0,0 +1,11 @@
+package com.gla;
+
+public class TextBook extends Book{
+    TextBook(int bookId, String title, double price){
+        super(bookId,title,price);
+    }
+    @Override
+    double calculateFine(int daysLate){
+        return daysLate*2;
+    }
+}
