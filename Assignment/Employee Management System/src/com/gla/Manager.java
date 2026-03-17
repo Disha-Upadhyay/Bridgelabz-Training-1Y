63f74b88c1626351c2375646a746b8e4848e38a5
Disha-Upadhyay <dishaupadhyay2007@gmail.com>
2026-03-17 08:36:13 +0530

    first commit

---
+++ Assignment/Employee Management System/src/com/gla/Manager.java
@@ -0,0 +1,15 @@
+package com.gla;
+
+public class Manager extends Employee {
+    String department;
+    public Manager(int empId,String name,String department){
+        super(empId, name);
+        this.department = department;
+    }
+    @Override
+    public String generateEmail(){
+        return name.toLowerCase()+"."+department.toLowerCase()+"@company.com";
+    }
+
+
+}
