7fd803a959cff9f00665830249c274787569e40a
Disha-Upadhyay <dishaupadhyay2007@gmail.com>
2026-04-05 12:16:44 +0530

    first commit

---
+++ Assignment/Wrapper Class/src/WrapperClasses/NormalQuestions/WrapperCollection.java
@@ -0,0 +1,27 @@
+package WrapperClasses.NormalQuestions;
+import java.util.ArrayList;
+public class WrapperCollection {
+    public static void main(String[] args) {
+        double[] prices = {10.5, 20.0, 35.75, 5.5};
+        ArrayList<Double> list = new ArrayList<>();
+
+        for (double price : prices) {
+            list.add(price);
+        }
+
+        double max = list.get(0);
+        double sum = 0;
+
+        for (Double p : list) {
+            if (p > max) {
+                max = p;
+            }
+            sum += p;
+        }
+
+        double avg = sum / list.size();
+
+        System.out.println("Highest price = " + max);
+        System.out.println("Average price = " + avg);
+    }
+}
