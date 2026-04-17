eaa4ce933e6fd8fb2a1720e2224cf0648e756bfe
Disha-Upadhyay <dishaupadhyay2007@gmail.com>
2026-04-17 22:29:40 +0530

    Disha Upadhyay

---
+++ Assignment/Assignment-7/src/com/gla/BookDetails/Book.java
@@ -0,0 +1,24 @@
+package com.gla.BookDetails;
+
+public class Book {
+    String title;
+    String author;
+    double price;
+
+    Book(){
+        title="Unknown";
+        author="Unknown";
+        price=0.0;
+    }
+    Book(String title,String author,double price){
+        this.title=title;
+        this.author=author;
+        this.price=price;
+    }
+    void display(){
+        System.out.println("title:"+title);
+        System.out.println("Author:"+author);
+        System.out.println("Price:"+price);
+
+    }
+}
